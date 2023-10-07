a0e793a8122992d60b2e8ebc3fc086a18675b3c5
doniyor <suratovdoniyor@gmail.com>
2023-10-07 16:04:07 +0500

    added env

--- root/settings.py
+++ root/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +83,11 @@ WSGI_APPLICATION = 'root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shop',
-        'USER': 'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':5432
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT':os.getenv("DB_PORT")
     }
 }
 
